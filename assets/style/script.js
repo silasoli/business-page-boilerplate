44d10023f59121e0d40f29f7e092a573b72b74c3
Silasoli <silas.oliveira@blockhub.builders>
2023-03-30 00:58:20 -0300

    feat: add imgs to banner, function to hidden and show banner and team section structure

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,1 +1,28 @@
-console.log("Hello world!")
+function hiddenCarousel() {
+  const carousel = window.document.getElementById("carousel");
+  carousel.style.display = "none";
+  const btnHiddenCarousel = window.document.getElementById(
+    "btn-hidden-carousel"
+  );
+  btnHiddenCarousel.style.display = "none";
+  const showCarousel = window.document.getElementById("btn-show-carousel");
+  showCarousel.style.display = "inline-block";
+}
+
+function showCarousel() {
+  const carousel = window.document.getElementById("carousel");
+  carousel.style.display = "block";
+  const showCarousel = window.document.getElementById("btn-show-carousel");
+  showCarousel.style.display = "none";
+  const btnHiddenCarousel = window.document.getElementById(
+    "btn-hidden-carousel"
+  );
+  btnHiddenCarousel.style.display = "inline-block";
+}
+
+// function changeTextByID(id, text) {
+//     const element = window.document.getElementById(id);
+//     element.textContent = text;
+// }
+
+// function
